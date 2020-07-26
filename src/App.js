bed368b27c063ec93e7826f8754628d43186d7f9
sarangmane607 <62138683+sarangmane607@users.noreply.github.com>
2020-07-26 20:52:01 +0530

    unused imports

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import TextField from '@material-ui/core/TextField';
 import Fab from '@material-ui/core/Fab';
-import FormatPaintIcon from '@material-ui/icons/FormatPaint';
 import './css/App.css';
 
 function TabPanel(props) {
